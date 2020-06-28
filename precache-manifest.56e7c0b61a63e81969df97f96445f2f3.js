bacd7521fa83b8f4c5bbe9b651aa3635561085b7
iamtigermaximus <46716968+iamtigermaximus@users.noreply.github.com>
2020-06-28 17:22:48 +0300

    Updates

---
+++ precache-manifest.56e7c0b61a63e81969df97f96445f2f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a105ca9ee008ebdaaf201bef51934c4a",
+    "url": "/Budget-App/index.html"
+  },
+  {
+    "revision": "31cc34ba87d0bbdcfba9",
+    "url": "/Budget-App/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "752e52b32e02b5c803e3",
+    "url": "/Budget-App/static/js/2.95002f45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget-App/static/js/2.95002f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31cc34ba87d0bbdcfba9",
+    "url": "/Budget-App/static/js/main.2a9a9c92.chunk.js"
+  },
+  {
+    "revision": "dd22bcf32b3b747efb6b",
+    "url": "/Budget-App/static/js/runtime-main.249db021.js"
+  }
+]);
